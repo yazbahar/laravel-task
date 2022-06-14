d0a2430571f36bbd36e15e4e2ce7c5246ba3aef6
yazbahar <ahmetyazbahar1@gmail.com>
2022-06-15 01:11:36 +0300

    added relations of products into related models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -14,4 +15,14 @@ class Product extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
